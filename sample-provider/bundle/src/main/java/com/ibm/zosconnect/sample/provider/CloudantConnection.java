446ed42994e8f6c3c88435569b9f77596305556a
Andrew Smithson <smithson@uk.ibm.com>
2018-01-31 11:29:48 +0000

    Load connection config from server.xml

--- sample-provider/bundle/src/main/java/com/ibm/zosconnect/sample/provider/CloudantConnection.java
+++ sample-provider/bundle/src/main/java/com/ibm/zosconnect/sample/provider/CloudantConnection.java
@@ -1,17 +1,49 @@
 package com.ibm.zosconnect.sample.provider;
 
 import org.osgi.service.component.ComponentContext;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.*;
 
 import java.util.Map;
 
 @Component(configurationPid = "com.ibm.zosconnect.sample.provider.connection", service = CloudantConnection.class, configurationPolicy = ConfigurationPolicy.REQUIRE)
 public class CloudantConnection {
 
+    private String id;
+    private String host;
+    private Integer port;
+
     @Activate
-    public void activate(ComponentContext cc, Map<String, Object> properties){
+    public void activate(ComponentContext cc, Map<String, Object> properties) {
         System.out.println("Activating connection: " + properties);
+    }
+
+    @Modified
+    public void modified(ComponentContext cc, Map<String, Object> properties) {
+
+    }
+
+    @Deactivate
+    public void deactivate(ComponentContext cc) {
+
+    }
+
+    private void updateConfig(Map<String, Object> properties) {
+        if (properties != null) {
+            id = (String) properties.get("id");
+            host = (String) properties.get("host");
+            port = (Integer) properties.get("port");
+        }
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public Integer getPort(){
+        return port;
     }
 }
